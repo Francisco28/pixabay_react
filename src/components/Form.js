8684edb31942b2bf59f37ba71da045170ff77e35
francisco28 <fgallegosgo@gmail.com>
2020-06-05 17:51:09 -0600

    validation Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Error from './Error';
 
 const Form = () => {
+
+    //hook - state "term"
+    const [ term, saveTerm ] = useState('');
+    const [ error, saveError ] = useState(false);
+
+    const SearchImages = e => {
+        e.preventDefault();
+        
+        //validate
+        if(term.trim() === '') {
+            saveError(true);
+            return;
+        }
+        saveError(false);
+
+        //submit the term of search towards main component
+
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={SearchImages}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input 
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Search a image"
-                        
+                        onChange={ e => saveTerm(e.target.value) }
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -22,6 +43,8 @@ const Form = () => {
                     />
                 </div>
             </div>
+
+            { error ? <Error message="Add a term of search" /> : null }
         </form>
      );
 }
