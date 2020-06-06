dc91d01a37b742fc9d29213dcf4aff696cf066de
francisco28 <fgallegosgo@gmail.com>
2020-06-06 14:24:54 -0600

    calculating the paginators

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   //state to current page
   const [ currentpage, saveCurrentPage ] = useState(1);
   //state to total of pages
-  const [ totalpages, saveTotalPages ] = useState(1);
+  const [ totalpages, saveTotalPages ] = useState(5);
 
 
   useEffect(() => {
@@ -37,6 +37,24 @@ function App() {
     consultAPI();
   }, [search]);
 
+  //define the previous page
+  const previousPage = () => {
+      const newCurrentPage = currentpage - 1;
+      //se detiene cuando es 0
+      if(newCurrentPage === 0) return;
+
+      saveCurrentPage(newCurrentPage);
+  }
+
+  //define the next page
+  const nextPage = () => {
+      const newCurrentPage = currentpage + 1;
+      
+      if(newCurrentPage > totalpages) return;
+
+      saveCurrentPage(newCurrentPage);
+  }
+
   return (
     <div className="container">
       <div className="jumbotron">
@@ -45,13 +63,26 @@ function App() {
         <Form 
           saveSearch={saveSearch}
         />
-      </div>
 
-      <div className="row justify-content-center">
-        <ListingImages 
-            images={images}
-        />
-      </div>
+        <div className="row justify-content-center">
+          <ListingImages 
+              images={images}
+          />
+        </div>
+
+        <button
+          type="button"
+          className="bbtn btn-info mr-1"
+          onClick={previousPage}
+        >&laquo;Previous</button>
+  
+        <button
+          type="button"
+          className="bbtn btn-info"
+          onClick={nextPage}
+        >Next&raquo;</button>
+
+      </div>      
     </div>
   );
 }
